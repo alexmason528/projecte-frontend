72d934127ea65b5cd411fae30beb035b5383fbbe
Alex Mason <alexmason528@hotmail.com>
2018-12-07 10:57:06 -0500

    Added UserStats component

---
+++ src/components/UserStats/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import bronzeStar from 'assets/images/bronze-star.png'
+
+export default class UserStats extends Component {
+  render() {
+    return (
+      <Container className="user-stats px-4">
+        <Row>
+          <Col md={6} className="py-4">
+            <h3 className="mt-0 font-weight-bold">STATS</h3>
+            <div className="d-flex justify-content-between">
+              <span>No estimations</span>
+              <span>4.332</span>
+            </div>
+            <div className="d-flex justify-content-between">
+              <span>Total amount</span>
+              <span>$1.323.010</span>
+            </div>
+            <div className="d-flex justify-content-between">
+              <span>Accuracy</span>
+              <span>7.5%</span>
+            </div>
+          </Col>
+          <Col md={6} className="py-4">
+            <h3 className="mt-0 font-weight-bold">RANK</h3>
+            Bronze Rater
+            <img className="user-stats-star" src={bronzeStar} alt="bronze-star" />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
